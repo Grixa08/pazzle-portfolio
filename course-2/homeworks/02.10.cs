27e8b57c02416fd6f4cac1c2e5889eca4c02954f
Grixa08 <braznik-03@list.ru>
2025-10-06 19:54:49 +0700

    дз

---
+++ course-2/homeworks/02.10.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+
+////work 1
+//Console.WriteLine("Введите градусы в цельсиях(через ',')");
+//float C = float.Parse(Console.ReadLine());
+//float F = C * 9 / 5 + 32;
+//Console.WriteLine(F);
+
+////work 2
+//Console.WriteLine("Введите целое число 'a'.");
+//int A = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите целое число 'b'.");
+//int B = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите целое число 'd'.");
+//int D = int.Parse(Console.ReadLine());
+//double sum = A + B + D;
+//double average = sum / 3;
+//Console.WriteLine(average);
+
+////work 3
+//Console.WriteLine("Введите первое число");
+//int num1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите второе число");
+//int num2 = int.Parse(Console.ReadLine());
+
+//double sum2 = num1 + num2;
+//double difference = num1 - num2;
+//double product = num1 * num2;
+//double quotient = num2 != 0 ? num1 / num2 : double.NaN;
+
+//Console.WriteLine($"Сумма: {num1} + {num2} = {sum}");
+//Console.WriteLine($"Разность: {num1} - {num2} = {difference}");
+//Console.WriteLine($"Произведение: {num1} * {num2} = {product}");
+//Console.WriteLine($"Частное: {num1} / {num2} = {quotient}");
--- course-2/practics/Program.cs
+++ course-2/practics/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+partial class Program
+{
+    static void Main()
+    {
+        // Задание 1
+        int a = 12;
+        int b = 5;
+
+        Console.WriteLine($"a = {a}, b = {b}");
+        Console.WriteLine($"Сумма: {a} + {b} = {a + b}");
+        Console.WriteLine($"Разность: {a} - {b} = {a - b}");
+        Console.WriteLine($"Произведение: {a} × {b} = {a * b}");
+        Console.WriteLine($"Частное: {a} ÷ {b} = {a / b}");
+        Console.WriteLine($"Остаток от деления: {a} % {b} = {a % b}");
+
+        // Задание 2
+        Console.Write("Введите ваше имя: ");
+        string name = Console.ReadLine() ?? "Гость";
+        Console.WriteLine($"Привет, {name}!");
+
+        // Задание 3
+        Console.WriteLine("Введите первое число: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Сумма: {num1 + num2}");
+
+        // Задание 4
+        Console.WriteLine("Введите ширину прямоугольника: ");
+        int width = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите длину прямоугольника: ");
+        int height = int.Parse(Console.ReadLine());
+        int area = width * height;
+        Console.WriteLine($"Площадь прямоугольника {width} × {height} = {area}");
+    }
+}
