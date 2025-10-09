63c645d3a2ce4e67b4b24a1315407920437fc725
Grixa08 <braznik-03@list.ru>
2025-10-09 21:15:09 +0700

    09.10 practics

--- course-2/practics/Program.cs
+++ course-2/practics/Program.cs
@@ -4,35 +4,86 @@ partial class Program
 {
     static void Main()
     {
-        // Задание 1
-        int a = 12;
-        int b = 5;
+        //practic 1
+        int[] num = { 1, 2, 3, 4, 5 };
 
-        Console.WriteLine($"a = {a}, b = {b}");
-        Console.WriteLine($"Сумма: {a} + {b} = {a + b}");
-        Console.WriteLine($"Разность: {a} - {b} = {a - b}");
-        Console.WriteLine($"Произведение: {a} × {b} = {a * b}");
-        Console.WriteLine($"Частное: {a} ÷ {b} = {a / b}");
-        Console.WriteLine($"Остаток от деления: {a} % {b} = {a % b}");
+        for (int i = 0; i < num.Length; i++)
+        {
+            Console.WriteLine("Элемент " + i + ": " + num[i]);
+        }
 
-        // Задание 2
-        Console.Write("Введите ваше имя: ");
-        string name = Console.ReadLine() ?? "Гость";
-        Console.WriteLine($"Привет, {name}!");
+        //practic 2
+        int[] grades = { 3, 4, 2, 5, 4, 4 };
+        Console.WriteLine("Оценки:");
+        for (int i = 0; i < grades.Length; i++)
+        {
+            Console.Write(grades[i] + " ");
+        }
+        Console.WriteLine();
+        int sum = 0;
+        for (int i = 0; i < grades.Length; i++)
+        {
+            sum += grades[i];
+        }
+        double average = (double)sum / grades.Length;
+        Console.WriteLine($"Среднее арифметическое: {average:F2}");
 
-        // Задание 3
-        Console.WriteLine("Введите первое число: ");
-        int num1 = int.Parse(Console.ReadLine());
-        Console.WriteLine("Введите второе число: ");
-        int num2 = int.Parse(Console.ReadLine());
-        Console.WriteLine($"Сумма: {num1 + num2}");
+        //practic 3
+        int[] ints = { 55, 35, 87, 79, 23, 39, 92, 1 };
+        int max = ints[0];
+        for (int i = 1; i < ints.Length; i++)
+        {
+            if (ints[i] > max)
+            {
+                max = ints[i];
+            }
+        }
+        Console.WriteLine(max);
 
-        // Задание 4
-        Console.WriteLine("Введите ширину прямоугольника: ");
-        int width = int.Parse(Console.ReadLine());
-        Console.WriteLine("Введите длину прямоугольника: ");
-        int height = int.Parse(Console.ReadLine());
-        int area = width * height;
-        Console.WriteLine($"Площадь прямоугольника {width} × {height} = {area}");
+        //practic 4
+        int[] numbers = { 2, 7, 15, 9, 3, 12, 3, 8, 4, 11 };
+
+        int chet = 0;
+        int nochet = 0;  
+
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                chet++;
+            }
+            else
+            {
+                nochet++;
+            }
+        }
+
+        Console.WriteLine("Четных = " + chet);
+        Console.WriteLine("Нечетных = " + nochet);
+
+        //practic 5
+        string[] fruits = { "яблоко", "банан", "груша", "авакадо", "манго" };
+
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+        
+        //practic 6
+        int[,] matrix = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
     }
 }
