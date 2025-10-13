ffb424786f890eb2f0c61b73f324ee004dc0080d
Grixa08 <braznik-03@list.ru>
2025-10-13 16:17:03 +0700

    homework09

---
+++ course-2/homeworks/homework09.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public static class homework09
+{
+    public static void Start()
+    {
+        //work1
+        int[] numbers = { 2, 5, 3, 5, 5, 4, 3};
+
+        int five = 0;
+        int two = 0;
+        foreach (int number in numbers) {
+            if (number == 5)
+            {
+                five++;
+            }
+            else if (number == 2)
+            {
+                two++;
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+        double average = (double)sum / numbers.Length;
+        Console.WriteLine($"Количество пятерок: {five} ");
+        Console.WriteLine($"Количество двоек: {two} ");
+        Console.WriteLine($"Среднее арифметическое: {average:F2}");
+
+        //work2
+        int[] num = { 3, 4, 7, 1, 56 };
+
+        Console.WriteLine("Массив в прямом порядке:");
+        for (int i = 0; i < num.Length; i++)
+        {
+            Console.Write(num[i] + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Массив в обратном порядке:");
+        for (int i = num.Length - 1; i >= 0; i--)
+        {
+            Console.Write(num[i] + " ");
+        }
+    }
+}
--- course-2/practics/06.10.cs
+++ course-2/practics/06.10.cs
@@ -1,48 +1,57 @@
-﻿//using System;
+﻿using System;
 
-////work1
-//int number = 10;
-//if (number > 0)
-//    Console.WriteLine("Число положительное");
-//else if (number < 0)
-//    Console.WriteLine("Число отрицательное");
-//else if (number == 0)
-//    Console.WriteLine("Число равно 0");
+public static class practic06
+{
+    public static void Start()
+    {
+        //work1
+        int number = 10;
+        if (number > 0)
+            Console.WriteLine("Число положительное");
+        else if (number < 0)
+            Console.WriteLine("Число отрицательное");
+        else if (number == 0)
+            Console.WriteLine("Число равно 0");
 
-////work 2
-//Console.WriteLine("Введите возраст");
-//int age = int.Parse(Console.ReadLine());
-//if (age > 18)
-//    Console.WriteLine("Вы совершеннолетний");
-//else
-//    Console.WriteLine("Вы не совершеннолетний");
+        //work 2
+        Console.WriteLine("Введите возраст");
+        int age = int.Parse(Console.ReadLine());
+        if (age >= 18)
+            Console.WriteLine("Вы совершеннолетний");
+        else
+            Console.WriteLine("Вы не совершеннолетний");
 
-////work3
-//int num = 7;
-//int sum = num % 2;
-//if (sum == 0)
-//    Console.WriteLine("Число четное");
-//else
-//    Console.WriteLine("Число нечетное");
+        //work3
+        int num = 7;
+        int sum = num % 2;
+        if (sum == 0)
+            Console.WriteLine("Число четное");
+        else
+            Console.WriteLine("Число нечетное");
 
-////work4
-//int a = 5;
-//int b = -2;
-//if (a > 0 && b > 0)
-//    Console.WriteLine("Оба числа четные");
-//if (a > 0 || b > 0)
-//    Console.WriteLine("Хотя бы одно положительное");
-//if (a < 0)
-//    Console.WriteLine("а не положительное");
+        //work4
+        int a = 5;
+        int b = -2;
+        if (a > 0 && b > 0)
+            Console.WriteLine("Оба числа четные");
+        if (a > 0 || b > 0)
+            Console.WriteLine("Хотя бы одно положительное");
+        if (a < 0)
+            Console.WriteLine("а не положительное");
 
-////work5
-//Console.WriteLine("Введите оценку");
-//int grage = int.Parse(Console.ReadLine());
-//if (grage < 3)
-//    Console.WriteLine("Не удовлетворительно");
-//else if (grage == 3)
-//    Console.WriteLine("Удовлетворительно");
-//else if (grage == 4)
-//    Console.WriteLine("Хорошо");
-//else
-//    Console.WriteLine("Отлично");
+        //work5
+        Console.WriteLine("Введите оценку");
+        int grage = int.Parse(Console.ReadLine());
+        if (grage < 3)
+            Console.WriteLine("Не удовлетворительно");
+        else if (grage == 3)
+            Console.WriteLine("Удовлетворительно");
+        else if (grage == 4)
+            Console.WriteLine("Хорошо");
+        else if (grage == 5)
+            Console.WriteLine("Отлично");
+        else
+            Console.WriteLine("Не верная оценка");
+
+    }
+}
